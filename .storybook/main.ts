f6015d57bc799c73bd7a2f83d268e343a85c21a1
Rao Nishchith Niranjan <nishchitrao5@gmail.com>
2023-11-04 19:23:06 +0530

    chore(sb): update SB dependencies

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,17 +1,13 @@
-import type { StorybookConfig, Options } from '@storybook/core-common';
+import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  core: { builder: 'webpack5' },
   stories: [
     '../src/lib/**/*.stories.mdx',
     '../src/lib/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  addons: ['@storybook/addon-essentials', '@nrwl/react/plugins/storybook'],
-  webpackFinal: async (config, { configType }: Options) => {
-    // add your own webpack tweaks if needed
-
-    return config;
-  },
+  core: { builder: '@storybook/builder-vite' },
+  addons: ['@storybook/addon-essentials'],
+  framework: '@storybook/react-vite',
 };
 
 module.exports = config;
